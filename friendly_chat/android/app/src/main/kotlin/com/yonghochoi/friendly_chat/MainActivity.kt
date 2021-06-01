5abd62648fb1102c805cfe49656246d196209eea
YonghoChoi <yongho1037@gmail.com>
2021-06-01 22:32:33 +0900

    초기 프로젝트 구성 및 텍스트 필드 추가

---
+++ friendly_chat/android/app/src/main/kotlin/com/yonghochoi/friendly_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yonghochoi.friendly_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ friendly_chat/lib/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    FriendlyChatApp(),
+  );
+}
+
+class FriendlyChatApp extends StatelessWidget {
+  const FriendlyChatApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FriendlyChat',
+      home: ChatScreen(),
+    );
+  }
+}
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _textController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FriendlyChat'),
+      ),
+      body: _buildTextComposer(),
+    );
+  }
+
+  Widget _buildTextComposer() {
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 8.0),
+      child: TextField(
+        controller: _textController,
+        onSubmitted: _handleSubmitted,
+        decoration: InputDecoration.collapsed(
+            hintText: 'Send a message'
+        ),
+      ),
+
+    );
+  }
+
+  void _handleSubmitted(String value) {
+    _textController.clear();
+  }
+}
