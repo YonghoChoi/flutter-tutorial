563ba67af8ed470761be6254eceb8317d93dc798
YonghoChoi <yongho1037@gmail.com>
2021-06-01 23:05:44 +0900

    채팅 기능 추가

--- friendly_chat/lib/main.dart
+++ friendly_chat/lib/main.dart
@@ -27,6 +27,8 @@ class ChatScreen extends StatefulWidget {
 
 class _ChatScreenState extends State<ChatScreen> {
   final _textController = TextEditingController();
+  final List<ChatMessage> _messages = [];
+  final FocusNode _focusNode = FocusNode();
 
   @override
   Widget build(BuildContext context) {
@@ -34,25 +36,98 @@ class _ChatScreenState extends State<ChatScreen> {
       appBar: AppBar(
         title: Text('FriendlyChat'),
       ),
-      body: _buildTextComposer(),
+      body: Column(
+        children: [
+          Flexible(
+              child: ListView.builder(
+            padding: EdgeInsets.all(8.0),
+            reverse: true,
+            itemBuilder: (_, int index) => _messages[index],
+            itemCount: _messages.length,
+          )),
+          Divider(height: 1.0),
+          Container(
+              decoration: BoxDecoration(color: Theme.of(context).cardColor),
+              child: _buildTextComposer()),
+        ],
+      ),
     );
   }
 
   Widget _buildTextComposer() {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 8.0),
-      child: TextField(
-        controller: _textController,
-        onSubmitted: _handleSubmitted,
-        decoration: InputDecoration.collapsed(
-            hintText: 'Send a message'
+    return IconTheme(
+      data: IconThemeData(color: Theme.of(context).accentColor),
+      child: Container(
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        child: Row(
+          children: [
+            Flexible(
+              // Flexible 사용 시 Row가 텍스트 필드의 크기를 자동으로 조정하여 버튼에서 사용하지 않는 남은 공간 사용
+              child: TextField(
+                controller: _textController,
+                onSubmitted: _handleSubmitted,
+                decoration:
+                    InputDecoration.collapsed(hintText: 'Send a message'),
+                focusNode: _focusNode,
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(horizontal: 4.0),
+              child: IconButton(
+                icon: const Icon(Icons.send),
+                onPressed: () => _handleSubmitted(_textController.text),
+              ),
+            ),
+          ],
         ),
       ),
-
     );
   }
 
-  void _handleSubmitted(String value) {
+  void _handleSubmitted(String text) {
     _textController.clear();
+    ChatMessage message = ChatMessage(text: text);
+
+    // setState로 데이터를 수정하면 위젯 트리의 이 부분이 변경 되었음을 감지하고 UI를 재빌드함
+    setState(() {
+      _messages.insert(0, message);
+    });
+    _focusNode.requestFocus();
+  }
+}
+
+// ignore: must_be_immutable
+class ChatMessage extends StatelessWidget {
+  final String text;
+  String _name = 'Yongho Choi';
+
+  ChatMessage({required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            margin: const EdgeInsets.only(right: 16.0),
+            child: CircleAvatar(child: Text(_name[0])),
+          ),
+          Column(
+            // crossAxisAlignement는 상위 위젯을 기준으로 하위 위젯을 배치
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                _name,
+                // Theme.of를 사용하면 앱의 기본 ThemData 객체를 제공
+                style: Theme.of(context).textTheme.headline4,
+              ),
+              Container(margin: EdgeInsets.only(top: 5.0), child: Text(text))
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
